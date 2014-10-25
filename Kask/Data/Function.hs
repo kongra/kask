cc9430fc2730761b4f646f6465febecc6dbb0aae
Konrad Grzanek <kongra@gmail.com>
2014-10-25 20:04:05 +0200

    compose and compose' work in the right order now. Added rcompose/rcompose'.

--- Kask/Data/Function.hs
+++ Kask/Data/Function.hs
@@ -19,17 +19,30 @@ module Kask.Data.Function
     (
       compose
     , compose'
+    , rcompose
+    , rcompose'
     ) where
 
 import Data.List (foldl')
 
 -- | Composes functions passed in a list. Uses foldl.
--- compose [f1, f2, ..., fn] = fn . ... . f2 . f1
+-- compose [f1, f2, ..., fn] = f1 . f2 . ... . fn
 compose :: [a -> a] -> a -> a
-compose = foldl (flip (.)) id
+compose = foldl (.) id
 {-# INLINE compose #-}
 
 -- | A strict version of compose. Uses fold'.
 compose' :: [a -> a] -> a -> a
-compose' = foldl' (flip (.)) id
+compose' = foldl' (.) id
 {-# INLINE compose' #-}
+
+-- | Composes functions passed in a list in a reversed order.
+-- Uses foldl. compose [f1, f2, ..., fn] = fn . ... . f2 . f1
+rcompose :: [a -> a] -> a -> a
+rcompose = foldl (flip (.)) id
+{-# INLINE rcompose #-}
+
+-- | A strict version of rcompose. Uses fold'.
+rcompose' :: [a -> a] -> a -> a
+rcompose' = foldl' (flip (.)) id
+{-# INLINE rcompose' #-}
