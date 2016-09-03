ee0b313972690fb3516106bd360c31f519387639
Konrad Grzanek <kongra@gmail.com>
2016-09-03 12:23:15 +0200

    Added printing abstraction module.

---
+++ src/Kask/Print.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE Safe #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : Kask.Print
+-- Copyright   : (c) 2016 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2016-09-03
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Imperative printing abstraction with IO and Text/String
+-- implementations
+------------------------------------------------------------------------
+module Kask.Print
+       (
+       )
+       where
