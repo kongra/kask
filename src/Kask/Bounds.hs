d836705726cd94f5f401a50025bea0aeb07141a6
Konrad Grzanek <kongra@gmail.com>
2016-09-04 20:18:48 +0200

    Modifications

---
+++ src/Kask/Bounds.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE Safe #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : Kask.Bounds
+-- Copyright   : (c) 2016 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2016-09-04
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Bounded numeric types
+------------------------------------------------------------------------
+module Kask.Bounds
+       (
+       )
+       where
+
+-- import Prelude hiding (Bounded)
+-- newtype Bounded a = Bounded { runUnbounded :: a } deriving Show;
+-- fromInt
+-- fromInteger
+-- fromDouble
