e5848d28227c67db18e975ea84c5a293e0f1d124
Konrad Grzanek <kongra@gmail.com>
2016-09-08 14:21:35 +0200

    Re-implemented tree printing using Kask.Print.

--- src/Kask/Data/Tree/Print.hs
+++ src/Kask/Data/Tree/Print.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE              Safe #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE        RankNTypes #-}
 ------------------------------------------------------------------------
 -- |
 -- Module      : Kask.Data.Tree.Print
@@ -14,140 +15,63 @@
 -- Haskell. Extensible, writes into monads with use of ShowS.
 ------------------------------------------------------------------------
 module Kask.Data.Tree.Print
-       (
+       ( Adjs
+       , Show
+       , Depth
+       , printTree
        )
        where
 
+import           Control.Monad (unless, forM_)
+import Data.Foldable (toList)
 import qualified Data.Text as T
--- import qualified Kask.Print as P
+import           Kask.Data.List (markLast)
+import qualified Kask.Print as P
+import           Prelude hiding (Show, show)
+import qualified Kask.Bounds as B
 
---     (
---       -- * Abstraction
---       ShowM
---     , showM
---     , Printer
---     , Merger
---     , Adjs
---     , Impl (..)
+type Adjs a t = Foldable t => a -> t a
+type Show a s = Symbolic s => a -> s
+type Depth = B.Bounded B.Positive Int
 
---       -- * Implementations
---     , io
---     , str
---     , istr
+printTree :: (P.Printable m s, Symbolic s, Foldable t) =>
+             a -> Adjs a t -> Show a s -> Maybe Depth -> m()
+printTree node adjacent show maxDepth =
+  doPrintTree node adjacent show (case maxDepth of
+                                     Just d  -> B.toUnbounded d - 1
+                                     Nothing -> maxBound)
+    0 [True] True
 
---       -- * Use
---     , Conf (..)
---     , printTree
---     )
---     where
+doPrintTree :: (P.Printable m s, Symbolic s, Foldable t) =>
+               a -> Adjs a t -> Show a s -> Int -> Int -> [Bool] -> Bool -> m()
+doPrintTree node adjacent show maxDepth level lastChildInfos isFirst = do
+  let s    = show node
+      pfx  = if isFirst then empty else eol
+      repr = if level == 0
+             then P.strCat [pfx, s]
+             else P.strCat [pfx, genIndent lastChildInfos, s]
 
--- import Control.Monad (forM)
--- import Control.Monad.Identity (Identity)
--- import Data.Maybe (fromMaybe)
---
--- import Kask.Data.List (markLast)
+  P.print repr
 
--- -- | A relation between the monad in which the print is performed, the
--- -- printed tree nodes, and the options of the process.
--- class (Monad m) => ShowM m o s where
---   -- | Converts the object of type s into ShowS in monad m. Uses options o.
---   showM :: o -> s -> m ShowS
+  unless (level == maxDepth) $ do
+    let children = toList $ adjacent node
+    forM_ (zip children (markLast children)) $ \(child, isLast) ->
+      doPrintTree child adjacent show maxDepth (level + 1 )
+        (isLast : lastChildInfos) False
 
--- -- | Embraces the process of converting ShowS into a target monadic
--- -- type.
--- type Printer m a = Monad m => ShowS -> m a
 
--- -- | Merges subcomponents of the process into a target monadic type.
--- type Merger m a = Monad m => [a] -> m a
-
--- -- | An implementation.
--- data Impl m a = Impl !(Printer m a) !(Merger m a)
-
--- -- | IO Implementation. Writes to the output stream using putStr.
--- io :: Impl IO ()
--- io = Impl (putStr . evalShowS) (const $ return ())
---        where evalShowS s = s ""
-
--- -- | Polymorphic implementation that prints to a resulting ShowS.
--- str :: Monad m => Impl m ShowS
--- str = Impl return (return . compose)
-
--- -- | Implementation that prints to a resulting Identity ShowS.
--- istr :: Impl Identity ShowS
--- istr = str
-
--- -- | A type of a function that takes an object s and returns its
--- -- children (in monad m). The argument o is options.
--- type Adjs m o s = Monad m => o -> s -> m [s]
-
--- -- | Configuration for the process of tree printing.
--- data Conf m a o s =
---   Conf
---   {
---     impl     :: !(Impl m a )
---   , adjs     :: !(Adjs m o s)
---   , maxDepth :: !(Maybe Int)
---   , opts     :: !o
---   }
-
--- -- | Prints the tree starting with the object (root of the tree).
--- printTree :: (ShowM m o s) =>
---         Conf m a o s
---         -> s          -- ^ Object to start with (root of the tree)
---         -> m a
--- printTree
---   Conf { impl     = Impl printer merger
---        , adjs     = adjs'
---        , maxDepth = maxDepth'
---        , opts     = opts' } s =
---     printImpl printer merger adjs' opts' (mdepth' - 1) s 0 [True] True
---   where
---     -- When no max depth specified, we use maxBound :: Int
---     mdepth  = fromMaybe maxBound maxDepth'
---     mdepth' = if mdepth < 1 then 1 else mdepth
-
--- printImpl :: (ShowM m o s) =>
---              Printer m a
---           -> Merger m a
---           -> Adjs m o s
---           -> o
---           -> Int
---           -> s -> Int -> [Bool] -> Bool -> m a
--- printImpl printer merger adjs' opts' mdepth
---           s level lastChildInfos isFirst = do
---   s' <- showM opts' s
---   let pfx  = if isFirst then empty else eol
---       repr = if level == 0
---                then compose [pfx, s']
---                else compose [pfx, genIndent lastChildInfos, s']
-
---   r <- printer repr
---   rs <- if level == mdepth
---           then return []  -- Do not recurse lower than maxDepth'
---           else do
---             let nextLevel  = level + 1
---             children <- adjs' opts' s
---             forM (zip children (markLast children)) $ \(child, isLast) ->
---               printImpl printer merger adjs' opts' mdepth
---                         child nextLevel (isLast:lastChildInfos) False
---   merger (r:rs)
-
--- genIndent :: [Bool] -> ShowS
--- genIndent [] = error "Empty genIndent argument !!!"
--- genIndent (isLast:lastChildInfos) = compose [prefix, suffix]
---   where
---     suffix             = if isLast then forLastChild else forChild
---     prefix             = rcompose (map indentSymbol (init lastChildInfos))
---     indentSymbol True  = emptyIndent
---     indentSymbol False = indent
-
--- indentSymbol :: Symbolic s => Bool -> s
--- indentSymbol True  = emptyIndent
--- indentSymbol False = indent
+genIndent :: Symbolic s => [Bool] -> s
+genIndent [] = empty -- should not happen anyway
+genIndent (isLast:lastChildInfos) = P.strCat [prefix, suffix]
+  where
+    indentSymbol True  = emptyIndent
+    indentSymbol False = indent
+    suffix  = if isLast then forLastChild else forChild
+    prefix  = P.strCat $ fmap indentSymbol $ reverse $ init lastChildInfos
 
 -- ASCII SYMBOLS
 
-class Symbolic s where
+class P.StrCat s => Symbolic s where
   indent       :: s
   emptyIndent  :: s
   forChild     :: s
