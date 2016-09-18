38f722314a3318c65a4d4957fc79a94acbe8b2dc
Konrad Grzanek <kongra@gmail.com>
2016-09-18 18:30:55 +0200

    Created Kask.Constr module.

---
+++ src/Kask/Constr.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE                  Safe #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : Kask.Constr
+-- Copyright   : (c) 2016 Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2016-09-19
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Constrained values constructors
+------------------------------------------------------------------------
+module Kask.Constr
+       (
+       )
+       where
+
+-- import qualified Kask
