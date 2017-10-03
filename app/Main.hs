4d75727f74f21e2b145e72d612b234471bde4922
kongra <kongra@gmail.com>
2017-10-03 22:36:19 +0200

    Fib structure performance test added.

--- app/Main.hs
+++ app/Main.hs
@@ -10,10 +10,11 @@ module Main (main) where
 -- import qualified Test006
 -- import qualified Test007
 -- import qualified Test008
-import qualified Test009
+-- import qualified Test009
+import qualified Test010
 
 main :: IO ()
-main =
+main = Test010.test1
   -- Test001.test1
   -- Test002.test1
   -- Test003.test5
@@ -24,4 +25,4 @@ main =
   -- Test007.test32
   -- Test008.test1
   -- Test008.test2
-  Test009.test1
+  -- Test009.test1
---
+++ app/Test010.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Safe #-}
+
+module Test010 where
+
+data FibCount = FibCount !Int !Int deriving Show
+
+fibcount :: Int -> FibCount
+fibcount 0 = FibCount 0 1
+fibcount 1 = FibCount 1 1
+fibcount n = FibCount (v1 + v2) (c1 + c2 + 1)
+  where
+    FibCount v1 c1 = fibcount (n-1)
+    FibCount v2 c2 = fibcount (n-2)
+
+test1 :: IO ()
+test1 = do
+  putStrLn (show (fibcount 42))
+  putStrLn (show (fibcount 43))
+  putStrLn (show (fibcount 44))
